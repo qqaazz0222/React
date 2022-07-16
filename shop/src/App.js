8397bd08104171af1031cc64db1125a0f842063b
qqaazz0222 <qqaazz0222@naver.com>
2022-07-16 14:17:22 +0900

    React shop 만들기
    2일차

--- shop/src/App.js
+++ shop/src/App.js
@@ -15,7 +15,7 @@ function App() {
           <Navbar.Brand href="/">ShoeShop</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/detail">Cart</Nav.Link>
+            <Nav.Link href="/cart">Cart</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
